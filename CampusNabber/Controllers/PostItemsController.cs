fefc922895200bb9392308f5f2d7cd89e4f65c1b
ryanscottmalmoe <ryanmalmoe@gmail.com>
2016-03-02 22:29:36 -0800

    Bug fixes
    Bug fixes in edit.cshtml

--- CampusNabber/Controllers/PostItemsController.cs
+++ CampusNabber/Controllers/PostItemsController.cs
@@ -10,6 +10,8 @@ using CampusNabber.Models;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using DatabaseCode.CNQueryFolder;
+using System.Data.Entity.Validation;
+using System.Diagnostics;
 
 namespace CampusNabber.Controllers
 {
@@ -152,7 +154,23 @@ namespace CampusNabber.Controllers
             if (ModelState.IsValid)
             {
                 db.Entry(postItem).State = EntityState.Modified;
-                db.SaveChanges();
+                
+                try
+                {
+                    db.SaveChanges();
+                }
+                catch (DbEntityValidationException dbEx)
+                {
+                    foreach (var validationErrors in dbEx.EntityValidationErrors)
+                    {
+                        foreach (var validationError in validationErrors.ValidationErrors)
+                        {
+                            Trace.TraceInformation("Property: {0} Error: {1}",
+                                                    validationError.PropertyName,
+                                                    validationError.ErrorMessage);
+                        }
+                    }
+                }
                 return RedirectToAction("Index");
             }
             return View(postItem);
