e3a0b24b51769db8320cc708ec6dc4ae0f92cfe3
iamaudrey <greataudrey@gmail.com>
2016-04-18 14:59:53 -0700

    Added BaseUser Role

--- CampusNabber/App_Start/RoleActions.cs
+++ CampusNabber/App_Start/RoleActions.cs
@@ -26,6 +26,10 @@ namespace CampusNabber.App_Start
             {
                 roleResult = roleManager.Create(new IdentityRole { Name = "Admin" });
             }
+            if(!roleManager.RoleExists("BaseUser"))
+            {
+                roleResult = roleManager.Create(new IdentityRole { Name = "BaseUser" });
+            }
 
 
             var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
