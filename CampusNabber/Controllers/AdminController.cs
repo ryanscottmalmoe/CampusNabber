065b6052605ae248f3fdaef6e6993556c3c2ac17
iamaudrey <iamaudrey@eagles.ewu.edu>
2016-04-21 16:24:16 -0700

    Attempting to add search user functionality

--- CampusNabber/Controllers/AdminController.cs
+++ CampusNabber/Controllers/AdminController.cs
@@ -3,11 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using CampusNabber.Models;
 
 namespace CampusNabber.Controllers
 {
     public class AdminController : Controller
     {
+        ApplicationUserManager _userManager;
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+        private CampusNabberEntities db = new CampusNabberEntities();
         //
         // GET: /Admin/
         [Authorize(Roles ="Admin")]
@@ -20,6 +36,14 @@ namespace CampusNabber.Controllers
         public ActionResult BlockUser()
         {
             return View();
+        }
+
+        [Authorize(Roles ="Admin")]
+        [HttpPost]
+        public ActionResult FindUser(string userEmail)
+        {
+            ApplicationUser result = UserManager.FindByEmail(userEmail);
+            return View(result);
         }
 	}
 }
