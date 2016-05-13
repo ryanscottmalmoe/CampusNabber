67eb6b107a372defb5a9bfcaa5f3e3b7ebb12b43
iamaudrey <iamaudrey@eagles.ewu.edu>
2016-05-12 20:55:59 -0700

    Implemented Add School feature

---
+++ CampusNabber/Utility/AdService.cs
@@ -0,0 +1,51 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+using CampusNabber.Models;
+using DatabaseCode.CNQueryFolder;
+using Microsoft.Ajax.Utilities;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Validation;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CampusNabber.Utility
+{
+    abstract class AdService
+    {
+        private static readonly string _awsAccessKey = "AKIAJ4CAE6M72TYTV2KA";
+        private static readonly string _awsSecretKey = "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";
+        private static readonly string _bucketName = "adphotos";
+
+        private static CampusNabberEntities db = new CampusNabberEntities();
+
+
+        public static void DeleteS3Photos(PostItemModel postItem)
+        {
+            List<string> photoStrings = GetS3Photos(postItem);
+            foreach (string photo in photoStrings)
+            {
+                IAmazonS3 client;
+                client = new AmazonS3Client(Amazon.RegionEndpoint.USWest2);
+
+                DeleteObjectRequest deleteObjectRequest =
+                    new DeleteObjectRequest
+                    {
+                        BucketName = _bucketName,
+                        Key = photo
+                    };
+
+                using (client = Amazon.AWSClientFactory.CreateAmazonS3Client(
+                     _awsAccessKey, _awsSecretKey))
+                {
+                    client.DeleteObject(deleteObjectRequest);
+                }
+            }
+        }
+    }
+}
