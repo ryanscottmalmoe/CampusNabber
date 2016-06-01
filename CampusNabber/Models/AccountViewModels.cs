ca621d3d222d7ba3dd41a767cda5067ddff155e5
seachowders <seachowders@gmail.com>
2016-05-31 21:34:12 -0700

    updates

--- CampusNabber/Models/AccountViewModels.cs
+++ CampusNabber/Models/AccountViewModels.cs
@@ -55,7 +55,7 @@ namespace CampusNabber.Models
         [Display(Name = "Username")]
         public string Email { get; set; }
 
-        [Required]
+       
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
@@ -74,7 +74,7 @@ namespace CampusNabber.Models
         [Display(Name = "Email")]
         public string Email { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Subcategory is required")]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
